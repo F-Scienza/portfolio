908841d3771731b13e319388a1fb18be5ed481fe
F-Scienza <cufa1905@gmail.com>
2023-12-19 11:14:21 -0300

    some changes

--- src/Components/Greeting/index.jsx
+++ src/Components/Greeting/index.jsx
@@ -1,7 +1,7 @@
 const Greeting = () => {
 	return (
-		<div className="flex flex-col w-screen p-8 pt-8 pb-8 mb-12 items-center justify-evenly bg-slate-900/20 rounded-lg lg:w-11/12 lg:flex-row">
-			<div className="h-96 w-11/12 max-w-md lg:w-1/2 pl-4 pr-4 flex flex-col justify-end lg:justify-center ">
+		<div className="flex flex-col w-screen p-8 pt-4 pb-8 mb-12 items-center justify-evenly bg-slate-900/20 rounded-lg lg:w-11/12 lg:flex-row">
+			<div className="h-96 w-11/12 max-w-md lg:w-1/2 lg:pl-4 lg:pr-4 flex flex-col justify-end lg:justify-center ">
 				<div className="w-1/3 border-t-2 border-cyan-500"></div>
 				<h1 className="text-2xl font-bold mb-2 tracking-widest">About me</h1>
 				<p>
@@ -13,15 +13,15 @@ const Greeting = () => {
 					through online resources such as YouTube
 				</p>
 			</div>
-			<div className="relative w-11/12 max-w-md h-96 lg:w-1/2 pl-4 pr-4 flex flex-col lg:justify-center">
+			<div className="relative w-11/12 max-w-md h-96 lg:w-1/2 lg:pl-4 lg:pr-4 flex flex-col lg:justify-center">
 				<p>
 					courses and official documentation. Later on, I enrolled in courses on
-					platforms like Platzi, Coderhouse, and Udemy, always complementing my
+					platforms like Platzi, Coderhouse and Udemy, always complementing my
 					learning with hands-on projects to apply my knowledge. I consider
 					myself a great team player, driven by curiosity, and a dedicated
 					self-learner.
 				</p>
-				<div className="absolute right-2 flex items-center bottom-12">
+				<div className="absolute right-2 flex items-center bottom-36 lg:bottom-12">
 					<span className="text-6xl font-black text-cyan-500">28</span>
 					<div className="flex flex-col justify-start text-justify font-bold ml-2 border-b-2 border-cyan-500">
 						<span>Years</span>
